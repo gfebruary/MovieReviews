0d39a0cd2c62eb74fa0f70c690d76afa8fc79f69
AlexJaegerPena <166617483+AlexJaegerPena@users.noreply.github.com>
2024-06-22 10:18:29 +0200

    backend, post and small changes

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const MovieDetails = ({ movies }) => {
   const { movieId } = useParams();
+  const [showTrailer, setShowTrailer] = useState(false);
+
+  // this useEffect ensures, that the page scrolls upwards as soon as the component is rendered
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [movieId]);
 
   if (!movies || movies.length === 0) {
     return <div>Loading...</div>;
@@ -29,24 +35,51 @@ const MovieDetails = ({ movies }) => {
 
   const trailerUrl = movie?.fields?.trailer?.fields?.file?.details?.url;
 
+  const handleMouseEnter = () => {
+    setShowTrailer(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTrailer(false);
+  };
+
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen mb-[300px] pb-20">
       <div
-        className="relative w-full h-1/2 bg-cover bg-top mx-auto"
+        className="absolute w-full h-1/2 bg-cover bg-top mx-auto"
         style={{
           backgroundImage: backgroundImageUrl
             ? `linear-gradient(to bottom, transparent, black), url(https:${backgroundImageUrl})`
             : "",
-        }}></div>
-      <div className="flex flex-col items-center mt-4 px-4">
+        }}
+      ></div>
+      <div className="mt-[25vw] flex flex-col items-center mt-4 px-4 relative">
         <div className="flex w-full max-w-4xl mx-auto">
           {posterImageUrl && (
-            <div className="w-1/3">
+            // trailer is played when hovering over poster
+            <div
+              className="w-1/3 relative"
+              style={{ maxWidth: "250px" }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
               <img
                 src={`https:${posterImageUrl}`}
                 alt="Movie Poster"
                 className="w-full h-auto"
+                style={{ maxWidth: "100%", height: "auto" }}
               />
+              {showTrailer && (
+                <iframe
+                  className="absolute top-0 left-0 w-full h-[100%]"
+                  src={`https://www.youtube.com/embed/_YUzQa_1RCE?autoplay=1&mute=1&cc_load_policy=0&controls=1`}
+                  frameBorder="0"
+                  allowFullScreen
+                ></iframe>
+              )}
+              {/* <p className="uppercase absolute bottom-0 left-0 right-0 px-2 py-24 bg-black bg-opacity-50 text-white">
+                Watch Trailer
+              </p> */}
             </div>
           )}
           <div className="w-2/3 pl-6">
